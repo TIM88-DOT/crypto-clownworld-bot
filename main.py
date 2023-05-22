cea608523a836a5620696c921dad8d3510f63403
N0name <63561853+TIM88-DOT@users.noreply.github.com>
2023-05-22 08:34:22 +0100

    big fix baby

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import sqlite3
-from telegram import Update
+from telegram import Update, ChatMember
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
 from threading import Thread
 from datetime import datetime, timedelta
 
 
 # Define a function for each thread to use
-def thread_function(user_id, username, skill):
+def thread_function(update, user_id, username, skill):
     # Create a new connection and cursor object for this thread
     conn = sqlite3.connect('users.db')
     c = conn.cursor()
@@ -17,62 +17,64 @@ def thread_function(user_id, username, skill):
     c.execute('CREATE TABLE IF NOT EXISTS skills (user_id INTEGER, skill TEXT, date_added TIMESTAMP DEFAULT CURRENT_TIMESTAMP, FOREIGN KEY (user_id) REFERENCES users(user_id))')
     c.execute('INSERT OR IGNORE INTO users (user_id, username) VALUES (?, ?)',
               (user_id, username))
-    c.execute('INSERT INTO skills (user_id, skill) VALUES (?, ?)',
-              (user_id, skill))
 
-    # Commit the changes and close the connection
-    conn.commit()
-    conn.close()
+    # Check if the user has already added 3 skills in the last 24 hours
+    c.execute('SELECT COUNT(*) FROM skills WHERE user_id = ? AND date_added > ?',
+              (user_id, datetime.now() - timedelta(hours=24)))
+    skill_count = c.fetchone()[0]
+
+    if skill_count < 3:
+        c.execute('INSERT INTO skills (user_id, skill) VALUES (?, ?)',
+                  (user_id, skill))
+        # Commit the changes and close the connection
+        conn.commit()
+        conn.close()
+        update.message.reply_text(
+            f"Shill '{skill}' added for {username} ({user_id})")
+    else:
+        conn.close()
+        update.message.reply_text(
+            "You have already added the maximum limit of 3 shills in the last 24 hours.")
+        
+        
+# Define the function for handling the /add_skill command
 
 def add_skill(update: Update, context: CallbackContext):
-    # Check if the message is a reply and save the skill from the replied message
+    # Check if the message is a reply
     if update.message.reply_to_message:
+        # Check if the user is a chat admin
+        chat_id = update.effective_chat.id
+        message_caller_id = update.effective_user.id
+        user_id = update.message.reply_to_message.from_user.id
+        username = update.message.reply_to_message.from_user.username
+        chat_member = context.bot.get_chat_member(chat_id, message_caller_id)
+        if chat_member.status == ChatMember.ADMINISTRATOR or chat_member.status == ChatMember.CREATOR or message_caller_id == user_id:
+            skill = update.message.reply_to_message.text
+
+            # Start a new thread to handle the database update
+            thread = Thread(target=thread_function,
+                            args=(update, user_id, username, skill))
+            thread.start()
+
+        else:
+            # If the user is not a chat admin, send an error message
+            update.message.reply_text(
+                "Only chat admins can save other users' shills.")
+    else:
+        # Get the user ID, username, and skill from the command arguments
         user_id = update.effective_user.id
         username = update.effective_user.username
-        message = update.message.reply_to_message.text or update.message.reply_to_message.caption
+        message = update.message.text
 
-        # Start a new thread to handle the database update
-        thread = Thread(target=thread_function,
-                        args=(user_id, username, message))
-        thread.start()
-
-        # Send a confirmation message
-        update.message.reply_text(
-            f"Skill '{message}' added for {username} ({user_id})")
-    else:
-        # Check if the message is in a group
-        if update.message.chat.type == "group" or update.message.chat.type == "supergroup":
-            # Get the user ID, username, and skill from the message text
-            user_id = update.effective_user.id
-            username = update.effective_user.username
-            message = update.message.text
-
-            # Exclude the command itself from being saved as a skill
-            if message.startswith('/save_shill'):
-                message = message[len('/save_shill'):].strip()
+        # Exclude the command itself from being saved as a skill
+        if message.startswith('/save_shill'):
+            message = message[len('/save_shill'):].strip()
 
             # Start a new thread to handle the database update
             thread = Thread(target=thread_function,
-                            args=(user_id, username, message))
+                            args=(update, user_id, username, message))
             thread.start()
 
-            # Send a confirmation message
-            update.message.reply_text(
-                f"Skill '{message}' added for {username} ({user_id})")
-        else:
-            # If the message is not a reply and not in a group, handle it as before
-            user_id = update.effective_user.id
-            username = update.effective_user.username
-            message = update.message.text
-
-            # Start a new thread to handle the database update
-            thread = Thread(target=thread_function,
-                            args=(user_id, username, message))
-            thread.start()
-
-            # Send a confirmation message
-            update.message.reply_text(
-                f"Skill '{message}' added for {username} ({user_id})")
 
 
 def list_skills(update: Update, context: CallbackContext):
@@ -97,8 +99,8 @@ def list_skills(update: Update, context: CallbackContext):
     conn.close()
 
     # Send the skill list as a message
-    update.message.reply_text(f"Latest shills added in the last 24 hours:\n{skill_list}")
-
+    update.message.reply_text(
+        f"Latest Shills in the last 24 hours:\n{skill_list}")
 
 
 # Create the bot and add the command handlers
@@ -109,5 +111,9 @@ dispatcher.add_handler(CommandHandler('shill_list', list_skills))
 dispatcher.add_handler(MessageHandler(Filters.text, add_skill))
 
 # Start the bot
-updater.start_polling()
-updater.idle()
+print("Bot started...")
+try:
+    updater.start_polling()
+    updater.idle()
+except Exception as e:
+    print("An exception occurred:", str(e))
