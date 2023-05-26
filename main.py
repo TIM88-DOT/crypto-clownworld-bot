c604735debb49431b4f5262577aaa87b8eacbabf
N0name <63561853+TIM88-DOT@users.noreply.github.com>
2023-05-26 15:20:09 +0100

    Update main.py

--- main.py
+++ main.py
@@ -97,7 +97,7 @@ def add_skill(update: Update, context: CallbackContext):
             else:
                 # If the user is not a chat admin or the original message sender, send an error message
                 update.message.reply_text(
-                    "Can't save other clowns shills unless you're a jannie.")
+                    "Only chat admins can save other users' shills unless you're a jannie.")
         else:
 
             # Check if the message is empty
@@ -117,18 +117,17 @@ def list_skills(update: Update, context: CallbackContext):
     conn = sqlite3.connect('users.db')
     c = conn.cursor()
 
-    # Get the datetime 24 hours ago and the current time
-    datetime_24h_ago = datetime.now() - timedelta(hours=24)
-    current_time = datetime.now()
+    # Get the current datetime
+    now = datetime.now()
 
     # Get all the users and their skills added in the last 24 hours from the database
     c.execute(
         'SELECT users.username, users.user_id, skills.skill, skills.message_reference, skills.date_added FROM users JOIN skills ON users.user_id = skills.user_id WHERE skills.date_added > ?',
-        (datetime_24h_ago,))
+        (now - timedelta(hours=24),))
     skills = c.fetchall()
 
-    # Sort the skills based on the usernames in ascending order and time elapsed in descending order
-    skills.sort(key=lambda x: (x[0], current_time - x[4]), reverse=True)
+    # Sort the skills based on the time elapsed in ascending order
+    skills.sort(key=lambda x: now - x[4])
 
     # Create a dictionary to store the shills for each user
     shills_by_user = {}
@@ -142,30 +141,27 @@ def list_skills(update: Update, context: CallbackContext):
                 shills_by_user[username] = []
             shills_by_user[username].append((message_reference, date_added))
 
-    # Create a formatted list of shills with sorted usernames, message references, and time elapsed
+    # Create a formatted list of shills with sorted usernames and message references
     skill_list = ''
-    previous_hours = -1
+    previous_elapsed_hours = None
 
-    for username, shills in shills_by_user.items():
-        skill_list += f'<b>{username}</b>\n'
-        for message_reference, date_added in shills:
-            hours_elapsed = (current_time - date_added).total_seconds() // 3600
-            if hours_elapsed != previous_hours:
-                if hours_elapsed >= 24:
-                    skill_list += '24 hours ago:\n'
-                else:
-                    skill_list += f'{int(hours_elapsed)} hours ago:\n'
-                previous_hours = hours_elapsed
-            if message_reference is not None:
-                skill_list += f'<a href=\'{message_reference}\'>{message_reference.split("/")[-1]}</a>\n'
-        skill_list += '\n'
+    for username in shills_by_user:
+        shills_by_user[username].sort(key=lambda x: now - x[1])
+        elapsed_hours = (now - shills_by_user[username][0][1]).seconds // 3600
+
+        if elapsed_hours != previous_elapsed_hours:
+            skill_list += f"\n<b>{elapsed_hours} {'hours' if elapsed_hours > 1 else 'hour'} ago:</b>\n"
+
+        skill_list += f"{username}: " + ', '.join(['<a href=\'' + ref[0] + '\'>' + ref[0].split(
+            "/")[-1] + '</a>' for ref in shills_by_user[username]]) + '\n'
+        previous_elapsed_hours = elapsed_hours
 
     # Close the connection
     conn.close()
 
-    # Send the shill list as a message with sorted usernames, message references, and time elapsed
+    # Send the shill list as a message with sorted usernames and message references
     update.message.reply_html(
-        f"<b>Latest Shills in the last 24 hours:</b>\n{skill_list}")
+        f"<b>Latest Shills in the last 24 hours:</b>{skill_list}")
 
 
 # Create the bot and add the command handlers
