c224804ac8f95a4d6d9db7ced2b3a03bd1b2b582
N0name <63561853+TIM88-DOT@users.noreply.github.com>
2023-05-26 14:41:59 +0100

    Update main.py

--- main.py
+++ main.py
@@ -117,38 +117,52 @@ def list_skills(update: Update, context: CallbackContext):
     conn = sqlite3.connect('users.db')
     c = conn.cursor()
 
-    # Get the datetime 24 hours ago
+    # Get the datetime 24 hours ago and the current time
     datetime_24h_ago = datetime.now() - timedelta(hours=24)
+    current_time = datetime.now()
 
     # Get all the users and their skills added in the last 24 hours from the database
     c.execute(
-        'SELECT users.username, users.user_id, skills.skill, skills.message_reference FROM users JOIN skills ON users.user_id = skills.user_id WHERE skills.date_added > ?',
+        'SELECT users.username, users.user_id, skills.skill, skills.message_reference, skills.date_added FROM users JOIN skills ON users.user_id = skills.user_id WHERE skills.date_added > ?',
         (datetime_24h_ago,))
     skills = c.fetchall()
 
-    # Sort the skills based on the usernames in ascending order
-    skills.sort(key=lambda x: x[0])
+    # Sort the skills based on the usernames in ascending order and time elapsed in descending order
+    skills.sort(key=lambda x: (x[0], current_time - x[4]), reverse=True)
 
     # Create a dictionary to store the shills for each user
     shills_by_user = {}
 
     # Group the shills by user
-    for username, user_id, skill, message_reference in skills:
+    for username, user_id, skill, message_reference, date_added in skills:
         if username or user_id:
             if not username:
                 username = user_id
             if username not in shills_by_user:
                 shills_by_user[username] = []
-            shills_by_user[username].append(message_reference)
+            shills_by_user[username].append((message_reference, date_added))
 
-    # Create a formatted list of shills with sorted usernames and message references
-    skill_list = '\n'.join(
-        [f"<b>{username}</b>" + ': ' + ', '.join(['<a href=\'' + ref + '\'>' + ref.split("/")[-1] + '</a>' for ref in shills_by_user[username]]) for username in shills_by_user])
+    # Create a formatted list of shills with sorted usernames, message references, and time elapsed
+    skill_list = ''
+    previous_hours = -1
+
+    for username, shills in shills_by_user.items():
+        skill_list += f'<b>{username}</b>\n'
+        for message_reference, date_added in shills:
+            hours_elapsed = (current_time - date_added).total_seconds() // 3600
+            if hours_elapsed != previous_hours:
+                if hours_elapsed >= 24:
+                    skill_list += '24 hours ago:\n'
+                else:
+                    skill_list += f'{int(hours_elapsed)} hours ago:\n'
+                previous_hours = hours_elapsed
+            skill_list += f'<a href=\'{message_reference}\'>{message_reference.split("/")[-1]}</a>\n'
+        skill_list += '\n'
 
     # Close the connection
     conn.close()
 
-    # Send the shill list as a message with sorted usernames and message references
+    # Send the shill list as a message with sorted usernames, message references, and time elapsed
     update.message.reply_html(
         f"<b>Latest Shills in the last 24 hours:</b>\n{skill_list}")
 
